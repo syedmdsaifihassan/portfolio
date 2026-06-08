5027b604c9f07d117ba3f3627f4523fad11b76f2
syedmdsaifihassan <syedmdsaifihassan@users.noreply.github.com>
2026-06-08 16:46:06 +0000

    deploy: 0871a34098acbd1e64cefda88cd78333ecd99eea

---
+++ _next/static/chunks/pages/_app-a98af5bcc6ac9461.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{2814:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(2296)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(2814),n(4006))),_N_E=_.O()}]);
